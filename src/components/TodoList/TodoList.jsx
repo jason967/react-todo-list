f6ab6b6cd572b2b67574f597335d977c621d0277
jaewoong-choi-bepluslab <choi.jw@bepluslab.co.kr>
2023-12-26 16:22:59 +0900

    Todo CRUD

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,17 +1,34 @@
 import React, { useState } from "react";
+import AddTodo from "../AddTodo/AddTodo";
+import Todo from "../Todo/Todo";
 
 export default function TodoList() {
   const [todos, setTodos] = useState([
     { id: "1", text: "장보기", status: "active" },
     { id: "2", text: "공부하기", status: "active" },
   ]);
+
+  const handleAdd = (todo) => setTodos([...todos, todo]);
+  const handleUpdate = (updated) =>
+    setTodos(todos.map((todo) => (todo.id === updated.id ? updated : todo)));
+  const handleDelete = (deleted) =>
+    setTodos(todos.filter((todo) => todo.id !== deleted.id));
+
   return (
     <section>
       <ul>
         {todos.map((item) => (
-          <li key={item.id}>{item.text}</li>
+          <Todo
+            key={item.id}
+            todo={item}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+          >
+            {item.text}
+          </Todo>
         ))}
       </ul>
+      <AddTodo onAdd={handleAdd} />
     </section>
   );
 }
