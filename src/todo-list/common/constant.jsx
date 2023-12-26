1007bc292c10930053e06385ecb287a758e29107
jaewoong-choi-bepluslab <choi.jw@bepluslab.co.kr>
2023-12-26 14:36:22 +0900

    Todo 리스트 구현

--- src/todo-list/common/constant.jsx
+++
@@ -1,7 +0,0 @@
-// export enum todoStatus {all="all",}
-
-export const todoStatus = {
-  all: "all",
-  active: "active",
-  completed: "completed",
-};
--- src/todo-list/header/components/TextButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function TextButton({ text, handleClick }) {
-  return (
-    <div>
-      <button onClick={handleClick}>{text}</button>
-    </div>
-  );
-}
