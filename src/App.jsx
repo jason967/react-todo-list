c8a83613baf93c09745c59911a89eed806e37d54
jaewoong-choi-bepluslab <choi.jw@bepluslab.co.kr>
2023-12-27 10:39:42 +0900

    darkMode context 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,21 @@ import "./App.css";
 import TodoList from "./components/TodoList/TodoList";
 import Header from "./components/Header/Header";
 import { useState } from "react";
+import { DarkModeProvider } from "./context/DarkModeContext";
 
 const filters = ["all", "active", "completed"];
 
 function App() {
   const [filter, setFilter] = useState(filters[0]);
   return (
-    <>
+    <DarkModeProvider>
       <Header
         filters={filters}
         filter={filter}
         onFilterChange={(filter) => setFilter(filter)}
       />
       <TodoList filter={filter} />
-    </>
+    </DarkModeProvider>
   );
 }
 
